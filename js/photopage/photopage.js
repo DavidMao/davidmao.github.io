5182a0b5565c8d98fb935996591f508ba55c4d88
David Mao <david.mao@gmail.com>
2018-02-20 23:49:29 -0800

    Photopage: Update stats

--- js/photopage/photopage.js
+++ js/photopage/photopage.js
@@ -83,14 +83,14 @@ var photopage = (function(self, $) {
       console.error(tulipLikeCountAnim.error);
     }
 
-    var morningViewCountAnim = new CountUp("morning-view-count", 0, 10, 0, 3);
+    var morningViewCountAnim = new CountUp("morning-view-count", 0, 11, 0, 3);
     if (!morningViewCountAnim.error) {
       morningViewCountAnim.start();
     } else {
       console.error(morningViewCountAnim.error);
     }
 
-    var morningDownloadCountAnim = new CountUp("morning-download-count", 0, 178, 0, 3);
+    var morningDownloadCountAnim = new CountUp("morning-download-count", 0, 185, 0, 3);
     if (!morningDownloadCountAnim.error) {
       morningDownloadCountAnim.start();
     } else {
